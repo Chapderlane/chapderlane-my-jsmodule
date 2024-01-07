5c6335d5b4f3c6e9dc27c06a8cf674d37d41676a
Chapderlane <154477996+Chapderlane@users.noreply.github.com>
2024-01-07 10:48:10 +0000

    Git commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,55 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  document.querySelector("#newExcuse").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
   console.log("Hello Rigo from the console!");
+};
+let generateExcuse = () => {
+  let pronoun = ["An", "The", "A", "Some"];
+  let subject = [
+    "astronaut",
+    "kangaroo",
+    "firefighter",
+    "coyote",
+    "actor",
+    "milkman"
+  ];
+  let action = [
+    "stole my",
+    "grabbed my",
+    "screamed at my",
+    "picked up my",
+    "threw my",
+    "wrote my"
+  ];
+  let object = ["purse", "bag", "book", "computer", "blanket", "towel", "car"];
+  let location = [
+    "in my house",
+    "at my school",
+    "at the park",
+    "at my job",
+    "in the shower",
+    "at the funeral home"
+  ];
+
+  let proIndex = Math.floor(Math.random() * pronoun.length);
+  let subIndex = Math.floor(Math.random() * subject.length);
+  let actIndex = Math.floor(Math.random() * action.length);
+  let objIndex = Math.floor(Math.random() * object.length);
+  let locIndex = Math.floor(Math.random() * location.length);
+
+  return (
+    pronoun[proIndex] +
+    " " +
+    subject[subIndex] +
+    " " +
+    action[actIndex] +
+    " " +
+    object[objIndex] +
+    " " +
+    location[locIndex]
+  );
 };
